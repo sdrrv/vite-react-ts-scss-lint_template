e08af397722d8af6c3d28187b0497b4d8840b0f7
sdrrv <duartecelvas@gmail.com>
2023-04-08 16:13:42 +0100

    templade finished

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Yarn + Vite + React + Ts + Scss+ Lint Template
-A Simple Template for a project using Yarn +Vite + React + Typescript + Scss + Eslint + Prettier 
+
+A Simple Template for a project using Yarn +Vite + React + Typescript + Scss + Eslint + Prettier
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), eslint()],
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  test: {
+    globals: true,
+    environment: "jsdom",
+    deps: {
+      registerNodeLoader: false,
+    },
+  },
+});
